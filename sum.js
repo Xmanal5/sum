c78c6238e2798600c7ec831e4800edbdec82e24b
Manal <123784788+Xmanal5@users.noreply.github.com>
2024-12-24 06:16:43 +0300

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,6 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const sum = num1 + num2;
+    document.getElementById("result").innerHTML = `The sum is: ${sum}`;
+}
